23ef1cfca9b796329ca8f1791a311d5ce9211b50
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-05-31 18:14:09 +0900

    dtoでバリデーションを追加

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,7 +1,21 @@
+import { Type } from 'class-transformer';
+import { IsInt, IsNotEmpty, IsString, MaxLength, Min } from 'class-validator';
+
 //クラスバリデーターを使うためにタイプエイリアスで定義
 export class CreateItemDto {
   // id: string; uuidで採番するため削除
+
+  @IsString() //文字列
+  @IsNotEmpty() //1文字以上
+  @MaxLength(40) //40文字以上
   name: string;
+
+  @IsInt() //数字
+  @Min(1) //1文字以上
+  @Type(() => Number) //プロパティをnumber型に認識するように
   price: number;
+
+  @IsString() //文字列
+  @IsNotEmpty() //1文字以上
   description: string;
 }
