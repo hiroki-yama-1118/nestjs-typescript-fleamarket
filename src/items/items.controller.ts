aa5480d2938eb95b116a1fb8ae275f642f477d6f
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-05-31 16:51:50 +0900

    CreateItemメソッド追加

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,4 +1,6 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ItemStatus } from './item-status.enum';
+import { Item } from './item.model';
 import { ItemsService } from './items.service';
 
 @Controller('items')
@@ -8,5 +10,23 @@ export class ItemsController {
   @Get()
   findAll() {
     return this.itemsService.findAll();
+  }
+  // リクエストボディから商品のパラメーターを取得
+  //  @Body()を使用 引数にキーを記述　横に変数名と型を記入
+  @Post()
+  create(
+    @Body('id') id: string,
+    @Body('name') name: string,
+    @Body('price') price: number,
+    @Body('description') description: string,
+  ): Item {
+    const item: Item = {
+      id,
+      name,
+      price,
+      description,
+      status: ItemStatus.ON_SALE,
+    };
+    return this.itemsService.create(item);
   }
 }
