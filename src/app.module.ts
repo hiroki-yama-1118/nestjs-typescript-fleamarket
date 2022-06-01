76d65890217d7ec5238d32e9f1b77f80887bb118
hiroki-yama-1118 <93690970+hiroki-yama-1118@users.noreply.github.com>
2022-06-01 10:31:51 +0900

    ormをインストールしてentity作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,20 @@
 import { Module } from '@nestjs/common';
 import { ItemsModule } from './items/items.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ItemsModule],
+  imports: [
+    ItemsModule,
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      database: 'postgres',
+      autoLoadEntities: true, //ORMにEntityを毎回追加することを省略できる
+    }),
+  ],
   controllers: [],
   providers: [],
 })
